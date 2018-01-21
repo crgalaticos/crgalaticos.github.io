dae508f807d408d2fabbd090b0f451e5bcd93b0b
Raphael Brasil <raphael.brasil@br.experian.com>
2018-01-21 18:13:40 -0200

    Configures babel and webpack

---
+++ build/main.bundle.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function HeaderBar() {
+    'use strict';
+
+    this.init = function () {
+        console.log('bla');
+    };
+
+    this.xunda = function () {
+        console.log('fasfsasfasf');
+    };
+
+    this.init();
+}
+
+var Xunda = new Xunda();
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+function HeaderBar(){
+    'use strict'
+
+    this.init = () => {
+        console.log('bla')
+    }
+
+    this.xunda = () => {
+        console.log('fasfsasfasf')
+    }
+
+    this.init()
+}
+
+var Xunda = new Xunda()
